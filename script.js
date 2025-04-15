ef2bcdef6d7562363026af07c9761a0c65528b0c
Duy <166584903+KDuy2009@users.noreply.github.com>
2025-04-15 18:07:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function openApp(app) {
+  switch(app) {
+    case 'facebook':
+      window.location.href = "https://www.facebook.com/share/1BcK3TmiAZ/"; // Hoặc link Facebook của bạn
+      break;
+    case 'zalo':
+      window.location.href = "https://zalo.me/0358757207"; // Hoặc link Zalo của bạn
+      break;
+    case 'instagram':
+      window.location.href = "https://www.instagram.com/nguyenkhanhduy3003?igsh=Zmpyd3ZmeWQxN21j"; // Hoặc link Instagram của bạn
+      break;
+    case 'tiktok':
+      window.location.href = "https://www.tiktok.com/@nguyen.duy029?_t=ZS-8vXkAC6O4Do&_r=1"; // Hoặc link TikTok của bạn
+      break;
+  }
+}
